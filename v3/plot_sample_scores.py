3a98015d5e0497b02e17f4e30fd8476a72d40865
GalZeevi <galzeevi@mail.tau.ac.il>
2023-03-18 16:14:26 +0200

    Add plot rewards and plot loss functions

--- v3/plot_sample_scores.py
+++ v3/plot_sample_scores.py
@@ -4,7 +4,8 @@ from checkpoint_manager_v3 import CheckpointManager
 from config_manager_v3 import ConfigManager
 import numpy as np
 
-CHECKPOINT_VER = 9
+CHECKPOINT_VER = 11
+RL_ENV_VER = 2
 k = 100
 view_size = ConfigManager.get_config('samplerConfig.viewSize')
 
@@ -28,5 +29,32 @@ def main():
     plt.show()
 
 
+DDQN = f'{k}_{30000}_{1020}_ddqn'
+
+
+def plot_loss():
+    ep_losses = CheckpointManager.load(f'{DDQN}_losses', CHECKPOINT_VER)
+    episodes = [pair[0] for pair in ep_losses]
+    losses = [pair[1] for pair in ep_losses]
+
+    plt.plot(episodes, losses)
+    plt.xlabel("episodes")
+    plt.ylabel("loss")
+    plt.title(f"ver{RL_ENV_VER} {DDQN} losses by episode")
+    plt.show()
+
+
+def plot_rewards():
+    ep_rewards = CheckpointManager.load(f'{DDQN}_rewards', CHECKPOINT_VER)
+    episodes = [pair[0] for pair in ep_rewards]
+    rewards = [pair[1] for pair in ep_rewards]
+
+    plt.plot(episodes, rewards)
+    plt.xlabel("episodes")
+    plt.ylabel("reward")
+    plt.title(f"ver{RL_ENV_VER} {DDQN} rewards by episode")
+    plt.show()
+
+
 if __name__ == '__main__':
-    main()
+    plot_loss()
