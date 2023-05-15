6a747877b7d5634bbcab1f99eb3596ac840e05c1
GalZeevi <galzeevi@mail.tau.ac.il>
2023-05-15 16:34:20 +0300

    Update greedy_sampler.py to use diversity

--- v3/greedy_sampler.py
+++ v3/greedy_sampler.py
@@ -10,7 +10,7 @@ from tqdm import tqdm, trange
 from checkpoint_manager_v3 import CheckpointManager
 from config_manager_v3 import ConfigManager
 from data_access_v3 import DataAccess
-from score_calculator import get_score2, get_threshold_score
+from score_calculator import get_score2, get_threshold_score, get_combined_score
 from train_test_utils import get_test_queries
 
 parser = argparse.ArgumentParser()
@@ -19,39 +19,56 @@ parser.add_argument("--queries", type=str, default='test', help="use test querie
 parser.add_argument("--pool_size", type=int, default=6, help="how many processes to use")
 parser.add_argument("--checkpoint", type=int, default=CheckpointManager.get_max_version(),
                     help="which checkpoint to use")
+parser.add_argument("--diversity_coeff", type=float, default=0.5, help="Weight to give diversity in score function.")
 args = parser.parse_args()
 print(f"Running with following CLI args: {args}")
 
 assert args.queries in ['test', 'train']
 schema = ConfigManager.get_config('queryConfig.schema')
 table = ConfigManager.get_config('queryConfig.table')
+pivot = ConfigManager.get_config('queryConfig.pivot')
 DataAccess()
 table_size = DataAccess.select_one(f'SELECT COUNT(1) AS table_size FROM {schema}.{table}')
 results = get_test_queries(args.checkpoint) if args.queries == 'test' else [np.arange(table_size)]
 TARGET_CHUNK_SIZE = 1000
 
 
-def worker_task(ids, sample, score):
+def get_tuples_from_ids(ids):
+    ids_db_format = ','.join([str(i) for i in ids])
+    return DataAccess.select(f'SELECT * FROM {schema}.{table} WHERE {pivot} IN ({ids_db_format})')
+
+
+def worker_task(tuples, sample, score):
     added_once = False
     new_tuple = None
-    for t_id in ids:
-        if t_id in sample:
+    sample_ids = [tup[pivot] for tup in sample]
+    for t in tuples:
+        t_id = t[pivot]
+        if t_id in sample_ids:
             continue
         if not added_once or \
-                get_score2(sample[:-1] + [t_id], queries=args.queries, checkpoint_version=args.checkpoint) > score:
+                get_combined_score(sample[:-1] + [t], queries=args.queries, alpha=(1 - args.diversity_coeff),
+                                   checkpoint_version=args.checkpoint) > score:
             if not added_once:
-                sample += [t_id]
+                sample += [t]
+                sample_ids += [t_id]
                 added_once = True
             else:
-                sample = sample[:-1] + [t_id]
-            new_tuple = t_id
-            score = get_score2(sample, queries=args.queries, checkpoint_version=args.checkpoint)
+                sample = sample[:-1] + [t]
+                sample_ids = sample_ids[:-1] + [t_id]
+
+            new_tuple = t
+            # score = get_score2(sample, queries=args.queries, checkpoint_version=args.checkpoint)
+            score = get_combined_score(sample, queries=args.queries,
+                                       alpha=(1 - args.diversity_coeff),
+                                       checkpoint_version=args.checkpoint)
     return new_tuple, score, os.getpid()
 
 
 def get_sample():
     sample = []
     score = 0.
+    print(f'Preparing tuples to process...')
     tuple_ids = reduce(np.union1d, tuple(results))
     print(f'Starting greedy sampler, {len(tuple_ids)} tuples to process')
     print('Initialising pool')
@@ -61,7 +78,8 @@ def get_sample():
 
     for _ in trange(args.k):
         new_tuple = None
-        chunks = [chunk for chunk in np.array_split(tuple_ids, np.ceil(len(tuple_ids) / TARGET_CHUNK_SIZE)) if
+        chunks = [get_tuples_from_ids(chunk) for chunk in
+                  np.array_split(tuple_ids, np.ceil(len(tuple_ids) / TARGET_CHUNK_SIZE)) if
                   len(chunk) > 0]
         print(f'Starting to work on {len(chunks)} tasks with {args.pool_size} workers', flush=True)
         worker_args = [(chunk, sample, score) for chunk in chunks]
@@ -70,9 +88,9 @@ def get_sample():
             if score < worker_score:
                 tqdm.write(
                     f'Sample size: {len(sample)}/{args.k}, \n' +
-                    f'current test score: {get_score2(sample, queries="test", checkpoint_version=args.checkpoint)}\n' +
+                    f'current test score: {get_score2([t[pivot] for t in sample], queries="test", checkpoint_version=args.checkpoint)}\n' +
                     f'current test_threshold score: '
-                    f'{get_threshold_score(sample, queries="test", checkpoint_version=args.checkpoint)}\n' +
+                    f'{get_threshold_score([t[pivot] for t in sample], queries="test", checkpoint_version=args.checkpoint)}\n' +
                     f', worker: {worker_pid}'
                 )
                 CheckpointManager.save(f'{args.k}-{args.queries}_greedy_sample', [sample, score],
